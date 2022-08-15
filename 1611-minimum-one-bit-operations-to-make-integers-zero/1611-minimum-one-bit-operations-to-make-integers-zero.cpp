31a83a664c27625309db92b3f94aa5f16027cf14
MOHD RIHAN <66411637+mikramrihan@users.noreply.github.com>
2022-08-15 15:50:31 +0530

    Time: 3 ms (45.15%), Space: 5.9 MB (80.10%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,34 +1,11 @@
 class Solution {
 public:
-//     ll solve(int i,int j){
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//         if(i==0){
-//             dp[i][j]=
-//         }
-//         else{
-            
-//         }
-//         return dp[i][j];
-//     }
     int minimumOneBitOperations(int n) {
-        vector<int> left(31,0);
-        left[0]=1;
-        left[1]=3;
-        // left[2]=;
-        for(int i=2;i<31;i++){
-            left[i]=left[i-1]<<1;
-        }
-        // debug(left);
-        int ans=0;
-        // 0 0 0 1 1 0 
-        for(int i=31;i>=0;i--){
-            if(n&(1<<i)){
-                ans+=(1<<i);
-                n^=left[i];
-            }
-        }
-        return ans;
-    }
+	     int zero = 0, one = 0;
+	     for (int i = 1; n; n >>= 1, i <<= 1) {
+		      one += i;
+		 if (n % 2) swap(one, zero);
+	}
+	return zero;
+  }
 };
