ababcc891ad8b32d682946192e1265abb62ec063
MOHD RIHAN <66411637+mikramrihan@users.noreply.github.com>
2022-08-16 08:39:06 +0530

    Time: 32 ms (81.69%), Space: 10.5 MB (91.21%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> mp;
+        int mp[26];
+        memset(mp,0,sizeof(mp));
         for(auto i: s){
-            mp[i]++;
+            mp[i-'a']++;
         }
         for(int i=0;i<s.size();i++){
-            if(mp.find(s[i])!=mp.end() && mp[s[i]]<2){
+            if(mp[s[i]-'a']<2){
                 return i;
             }
         }
