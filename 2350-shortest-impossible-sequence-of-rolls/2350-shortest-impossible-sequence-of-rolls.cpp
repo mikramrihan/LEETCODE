ec4175b14854125bd50a7d097d2354cce8f6d81d
MOHD RIHAN <66411637+mikramrihan@users.noreply.github.com>
2022-08-22 00:48:26 +0530

    Time: 384 ms (56.66%), Space: 119.4 MB (16.56%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int shortestSequence(vector<int>& rolls, int k) {
         int n=rolls.size();
-        unordered_map<int,int> mp;
+        set<int> s;
         int level=1;
         for(int i=0;i<n;i++){
-            mp[rolls[i]]++;
-            if(mp.size()==k){
+            s.insert(rolls[i]);
+            if(s.size()==k){
                 level++;
-                mp.clear();
+                s.clear();
             }
         }
         return level;
